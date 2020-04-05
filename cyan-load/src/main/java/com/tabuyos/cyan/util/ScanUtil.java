5789672c6bd7f30aa07951a9aef396dbbefd760c
Tabuyos <Tabuyos@outlook.com>
2020-04-05 15:25:20 +0800

    Merge 0.1.0 to master.

--- cyan-load/src/main/java/com/tabuyos/cyan/util/ScanUtil.java
+++ cyan-load/src/main/java/com/tabuyos/cyan/util/ScanUtil.java
@@ -1,6 +1,5 @@
 package com.tabuyos.cyan.util;
 
-import com.alibaba.fastjson.JSONObject;
 import com.tabuyos.cyan.config.AutowireJar;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Type;
@@ -12,8 +11,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
@@ -30,7 +27,6 @@ import java.util.jar.JarFile;
 public class ScanUtil {
     private List<String> paths = new ArrayList<>();
     private List<String> names = new ArrayList<>();
-//    private final Map<String, Map<String, String>> map = new HashMap<>();
 
 
     public ScanUtil() {
@@ -53,7 +49,6 @@ public class ScanUtil {
                 JarFile jarFile = new JarFile(path);
                 Enumeration<JarEntry> enumeration = jarFile.entries();
                 AutowireJar.getJarInfoMap().put(jarName, FileUtil.normalize(path));
-//                Map<String, String> classMap = new HashMap<>();
                 while (enumeration.hasMoreElements()) {
                     JarEntry jarEntry = enumeration.nextElement();
                     String name = jarEntry.getName();
@@ -61,40 +56,15 @@ public class ScanUtil {
                         names.add(name);
                         String className = FileUtil.getFileName(name, Constant.SUFFIX_CLASS);
                         String classPath = FileUtil.getClassPackage(name);
-//                        classMap.put(className, classPath);
                         AutowireJar.getClassInfoMap().put(className, classPath);
                     }
                 }
-//                map.put(jarName, classMap);
                 jarFile.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
-//    public void parseClass() {
-//        try {
-//            File file = new File("F:/Temp/asm/fastjson-1.2.68.jar");//jar包的路径
-//            System.out.println(file.toURI());
-//            URL url = file.toURI().toURL();
-//            System.out.println(url);
-//            ClassLoader loader = new URLClassLoader(new URL[]{url});//创建类加载器
-//            Class<?> cls = loader.loadClass("com.alibaba.fastjson.JSONObject");//加载指定类，注意一定要带上类的包名
-//            System.out.println(cls.getResource("").getPath());
-//            System.out.println(cls.getSimpleName());
-//            Method[] methods = cls.getMethods();//方法名和对应的各个参数的类型
-//            System.out.println("----------------------------------------------------");
-//            for (Method method : methods) {
-//                System.out.println(Modifier.toString(method.getModifiers()) + " " + method.getReturnType().getName() + " " + method.getName() + " " + Arrays.toString(method.getParameters()));
-////                System.out.println(method.getName());
-//            }
-////            Object o=method.invoke(null,"chen",Integer.valueOf(10),"0");//调用得到的上边的方法method(静态方法，第一个参数可以为null)
-////            System.out.println(String.valueOf(o));//输出"000chen000","chen"字符串两边各加3个"0"字符串
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     // use asm parse class
     public void parseClass(String... classPaths) {
@@ -123,7 +93,6 @@ public class ScanUtil {
                     }
                     assert inputStream != null;
                     ClassReader reader = new ClassReader(inputStream);
-//                ClassReader reader = new ClassReader(new FileInputStream(new File("F:/Temp/asm/asm.class")));
                     ClassNode cn = new ClassNode();
                     reader.accept(cn, 0);
                     String superName = cn.superName;
@@ -136,34 +105,14 @@ public class ScanUtil {
                     Map<String, String> methodMap = new RepeatHashMap();
                     List<Map<String, String>> list = new ArrayList<>();
 
-//                    // handler class
-//                    AutowireJar.getClassInfoMap().put(className, classPackage);
-
-//                    // handler superclass
-//                    System.out.println("====================Tabuyos-SuperClass====================");
-//                    System.out.println("Super Class: " + superName.replace("/", "."));
-//                    System.out.println("====================Tabuyos-SuperClass====================");
-//
-//                    // handler interface
-//                    System.out.println("====================Tabuyos-Interface====================");
-//                    for (String anInterface : interfaces) {
-//                        System.out.println("Interface Class: " + anInterface.replace("/", "."));
-//                    }
-//                    System.out.println("====================Tabuyos-Interface====================");
-
-                    // handler field
-//                    System.out.println("====================Tabuyos-Field====================");
                     for (FieldNode fieldNode : fields) {
                         String fieldString = ModifierUtil.getModifier(fieldNode.access) + Constant.SPACE +
                                 Type.getType(fieldNode.desc).getClassName() + Constant.SPACE +
                                 fieldNode.name + Constant.SPACE;
-//                        System.out.println(fieldString.trim());
                         fieldMap.put(fieldNode.name, fieldString.trim());
                     }
-//                    System.out.println("====================Tabuyos-Field====================");
 
                     // handler method
-//                    System.out.println("====================Tabuyos-Method====================");
                     for (MethodNode methodNode : methodNodes) {
                         StringBuilder methodString = new StringBuilder();
                         methodString.append(ModifierUtil.getModifier(methodNode.access)).append(Constant.SPACE);
@@ -188,13 +137,11 @@ public class ScanUtil {
                             methodString.append(parameterString.substring(0, parameterString.lastIndexOf(Constant.COMMA)));
                         }
                         methodString.append(Constant.RIGHT_PARENTHESIS);
-//                        System.out.println(methodString.toString().trim());
                         methodMap.put(methodName, methodString.toString().trim());
                     }
                     list.add(fieldMap);
                     list.add(methodMap);
                     AutowireJar.getClassMap().put(className, list);
-//                    System.out.println("====================Tabuyos-Method====================");
                 }
                 AutowireJar.getJarMap().put(jarName, AutowireJar.getClassMap());
             } catch (Exception e) {
@@ -202,8 +149,4 @@ public class ScanUtil {
             }
         }
     }
-
-//    public Map<String, Map<String, String>> getMap() {
-//        return map;
-//    }
 }
